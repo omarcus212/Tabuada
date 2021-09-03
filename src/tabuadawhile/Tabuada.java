23be8b8c92a43dd718adf2c1c8ce059d50fd76a7
MARCUS VINNÍCIUS RODRIGUES DE SOUZA <marcusvinniciusouza@gmail.com>
2021-09-03 16:46:35 -0300

    conclusão tabuada

---
+++ src/tabuadawhile/Tabuada.java
@@ -0,0 +1,25 @@
+package tabuadawhile;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+	 public static void main(String[] args) {
+		
+		 Scanner leitor = new Scanner (System.in);
+		 
+		 int maxmultiplicador,multiplicador;
+		 
+		 System.out.print("TABUADO DO : ");
+		 multiplicador = leitor.nextInt();
+		 System.out.println("-------------");
+		 System.out.print("DETERMINE O MULTIPLICADOR MAXIMO : ");
+		 maxmultiplicador = leitor.nextInt();
+		 System.out.println("-------------");
+		 
+		 
+		 
+		 
+		 
+	}
+}
